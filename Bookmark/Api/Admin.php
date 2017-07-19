4e498fb39fe94d9e39640351f0fb5ffd05076f26
LW <vv15802702853@163.com>
2017-07-20 00:26:07 +0800

    更新Data、Model

--- Bookmark/Api/Admin.php
+++ Bookmark/Api/Admin.php
@@ -7,22 +7,18 @@ class Api_Admin extends PhalApi_Api
     public function getRules() {
         return array(
             'insert' => array(
-            	'pid' 			=> array('name' => 'pid', 'type' => 'int', 'default' => 0, 'desc' => '文件夹目录ID'),
-            	'name' 			=> array('name' => 'name', 'require' => true, 'desc' => '书签英文'),
-            	'title' 		=> array('name' => 'title', 'require' => true, 'desc' => '书签名'),
+                'url'           => array('name' => 'url', 'require' => true, 'desc' => '书签地址'),
+                'name'          => array('name' => 'name', 'require' => true, 'desc' => '书签英文'),
+                'wall'          => array('name' => 'wall', 'type' => 'boolean', 'default' => false, 'desc' => '是否需要翻墙'),
             	'color'			=> array('name' => 'color', 'default' => '#9E9E9E', 'desc' => '书签背景色块'),
-            	'icon' 			=> array('name' => 'icon', 'default' => 'icons:folder', 'desc' => '书签图标'),
-            	'url' 			=> array('name' => 'url', 'desc' => '书签地址'),
-            	'url_name' 		=> array('name' => 'url_name', 'desc' => '书签地址展示名'),
-            	'image_id'		=> array('name' => 'image_id', 'type' => 'int', 'min' => 1, 'max' => 11, 'desc' => '书签背景图ID'),
-            	'image_url'     => array('name' => 'image_url', 'desc' => '书签背景图URL'),
-            	'image_preview' => array('name' => 'image_preview', 'desc' => '书签缩略图'),
-            	'description'   => array('name' => 'description', 'desc' => '书签备注'),
-            	'root'		 	=> array('name' => 'root', 'type' => 'boolean', 'require' => true, 'default' => false, 'desc' => '是否为根目录'),
-            	'folder'		=> array('name' => 'folder', 'type' => 'boolean', 'require' => true, 'default' => false, 'desc' => '是否为文件夹'),
-            	'wall'			=> array('name' => 'wall', 'type' => 'boolean', 'require' => true, 'default' => false, 'desc' => '是否需要翻墙'),
-                'weight' 		=> array('name' => 'weight', 'type' => 'int', 'require' => true, 'desc' => '书签权重'),
+                'description'   => array('name' => 'description', 'desc' => '书签备注'),
+            	'preview'       => array('name' => 'image_preview', 'desc' => '书签缩略图'),
+                'image_id'      => array('name' => 'image_id', 'type' => 'int', 'min' => 1, 'max' => 11, 'desc' => '书签背景图ID'),
+                'image_url'     => array('name' => 'image_url', 'desc' => '书签背景图URL'),
                 'updata' 		=> array('name' => 'updata', 'type' => 'date', 'format' => 'timestamp', 'desc' => '更新时间'),
+            ),
+            'import' => array(
+                'data' => array('name' => 'data', 'type' => 'array', 'require' => true, 'desc' => '更新时间'),
             ),
         );
     }
@@ -38,21 +34,14 @@ class Api_Admin extends PhalApi_Api
     	$rs = array('code' => 200, 'msg' => '', 'info' => array());
 
     	$newData = array(
-    	    'pid' 			=> $this->pid,
+            'url'           => $this->url,
     	    'name' 			=> $this->name,
-    	    'title' 		=> $this->title,
-    	    'color'			=> $this->color,
-    	    'icon' 			=> $this->icon,
-    	    'url' 			=> $this->url,
-    	    'url_name' 		=> $this->url_name,
-    	    'image_id' 		=> $this->image_id,
-    	    'image_url' 	=> $this->image_url,
-    	    'image_preview' => $this->image_preview,
-    	    'description' 	=> $this->description,
-    	    'root' 			=> $this->root,
-    	    'folder' 		=> $this->folder,
-    	    'wall' 			=> $this->wall,
-    	    'weight' 		=> $this->weight,
+            'wall'          => $this->wall,
+            'color'         => $this->color,
+            'description'   => $this->description,
+    	    'preview'       => $this->preview,
+            'image_id'      => $this->image_id,
+            'image_url'     => $this->image_url,
     	    'updata' 		=> $_SERVER['REQUEST_TIME'],
     	);
 
@@ -61,5 +50,26 @@ class Api_Admin extends PhalApi_Api
 
     	$rs['id'] = $id;
     	return $rs;
+    }
+
+    /**
+     * JSON文件导入
+     * @desc 读取JSON文件数据向数据库插入一条纪录数据
+     * @return [type] [description]
+     */
+    public function import()
+    {
+        $rs = array('code' => 200, 'msg' => '', 'info' => array());
+
+        $newData = $this->data;
+        foreach ($newData as $key => $value) {
+            # code...
+        }
+
+        $domain = new Domain_Admin();
+        $id = $domain->insert($newData);
+
+        $rs['id'] = $id;
+        return $rs;
     }
 }
--- Data/opixer-phalapi.sql
+++ Data/opixer-phalapi.sql
@@ -21,22 +21,38 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `opixer_bookmark`;
 CREATE TABLE `opixer_bookmark` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '书签ID',
-  `pid` int(11) unsigned DEFAULT NULL COMMENT '文件夹目录ID',
+  `url` varchar(255) NOT NULL COMMENT '书签地址',
   `name` varchar(255) NOT NULL COMMENT '书签英文',
-  `title` varchar(255) NOT NULL COMMENT '书签名',
+  `wall` tinyint(4) DEFAULT NULL COMMENT '是否需要翻墙',
   `color` varchar(255) DEFAULT NULL COMMENT '书签背景色块',
-  `icon` varchar(255) DEFAULT NULL COMMENT '书签图标',
-  `url` varchar(255) DEFAULT NULL COMMENT '书签地址',
-  `url_name` varchar(255) DEFAULT NULL COMMENT '书签地址展示名',
+  `description` varchar(255) DEFAULT NULL COMMENT '书签备注',
+  `preview` varchar(255) DEFAULT NULL COMMENT '缩略图',
   `image_id` int(11) DEFAULT NULL COMMENT '书签背景图ID',
   `image_url` varchar(255) DEFAULT NULL COMMENT '书签背景图URL',
-  `image_preview` varchar(255) DEFAULT NULL COMMENT '书签缩略图',
-  `description` varchar(255) DEFAULT NULL COMMENT '书签备注',
-  `root` tinyint(4) NOT NULL COMMENT '是否为根目录',
-  `folder` tinyint(4) NOT NULL COMMENT '是否为文件夹',
-  `wall` tinyint(4) DEFAULT NULL COMMENT '是否需要翻墙',
-  `weight` int(11) NOT NULL COMMENT '书签权重',
   `updata` int(10) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+-- ----------------------------
+-- Table structure for opixer_bookmark_link
+-- ----------------------------
+DROP TABLE IF EXISTS `opixer_bookmark_link`;
+CREATE TABLE `opixer_bookmark_link` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '标签ID',
+  `name` varchar(255) DEFAULT NULL COMMENT '标签名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- ----------------------------
+-- Table structure for opixer_bookmark_link_group
+-- ----------------------------
+DROP TABLE IF EXISTS `opixer_bookmark_link_group`;
+CREATE TABLE `opixer_bookmark_link_group` (
+  `id` int(11) NOT NULL,
+  `bid` int(11) NOT NULL COMMENT '书签ID',
+  `lid` int(11) NOT NULL COMMENT '标签ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
